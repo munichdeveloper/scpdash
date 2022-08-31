797a0c643f49490773c8cd7dbb038021899d457e
munich developer <johannes.hinkov@gmail.com>
2022-08-31 22:44:17 +0200

    update host page

--- pages/host.js
+++ pages/host.js
@@ -6,7 +6,7 @@ Host.getInitialProps = async (ctx) => {
     const res = await fetch('http://localhost:4280/host', {
         headers: {
             'User-Agent': 'ScPrime-Agent',
-            'Authentication': 'Basic ' + btoa(':015ebec5fa1a997957fc1e43f8cbdfe0')
+            'Authentication': 'Basic ' + Buffer.from(':015ebec5fa1a997957fc1e43f8cbdfe0', 'base64')
         }
     })
     console.log(res)
