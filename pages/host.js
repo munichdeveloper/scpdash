f93bcb2462917d6ba535e9c74b33251ce88ca5b6
munich developer <johannes.hinkov@gmail.com>
2022-08-31 22:24:12 +0200

    update host page

---
+++ pages/host.js
@@ -0,0 +1,11 @@
+function Host({ netaddress }) {
+    return <div>Net Address: {netaddress}</div>
+}
+
+Host.getInitialProps = async (ctx) => {
+    const res = await fetch('http://localhost:4280/host')
+    const json = await res.json()
+    return { netaddress: json.externalsettings.netaddress }
+}
+
+export default Host
